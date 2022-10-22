c2e710957772f9a60fb83b710ccebaea6f1643b9
smileX2_merries <s.arai@newell-productions.com>
2022-10-23 07:28:36 +0900

    pre-release (#1)
    * update: site meta data
    
    * update: gitignore
    
    * update: 基本スタイル
    
    * update: GA tag, meta
    
    * update: header
    
    * add: key-visual iamges
    
    * create: sectionごとのpug
    
    * update: header scss
    
    * update: 確認コメント
    
    * update: footer
    
    * update: section keyvisual
    
    * update: set main color to the var file
    
    * fix: sp画像はspへ
    
    * fix: name
    
    * update: present
    
    * fix: sp keyvid
    
    * delete: 不要のため
    
    * update: 全セクション読み込み
    
    * create: 表示切り替え用SCSS
    
    * update: breakpoint
    
    * update: contents 表示用
    
    * update: component読み込み
    
    * update: style files
    
    * update: apply sp
    
    * create: content scss
    
    * update: about sp
    
    * fix: button
    
    * create: setting pug
    
    * update: about setting 適用
    
    * fix: img path
    
    * update: special contents except GIF
    
    * update: sp content
    
    * update: terms
    
    * update: link
    
    * fix: space
    
    * update: balloon
    
    * update: quiz
    
    * fix: jsはビルドしない
    
    * add: fonts
    
    * update: about pc
    
    * fix: blockだとややこしい
    
    * create: img 4 pc

--- src/assets/scripts/main.ts
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  window.setInterval(() => {
-    document.body.style.borderColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
-  }, 3000)
-})
