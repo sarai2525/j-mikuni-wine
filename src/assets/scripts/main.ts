4c9f78acb58d82bd8d968934d3ce7c3e03f7a575
smileX2_merries <s.arai@newell-productions.com>
2022-10-25 01:17:53 +0900

    test release (#2)
    * update: site meta data
    
    * update: gitignore
    
    * update: 基本スタイル
    
    * update: GA tag, meta
    
    * update: header
    
    * add: key-visual iamges
    
    * create: sectionごとのpug
    
    * update: header scss
    
    * update: 確認コメント
    
    * update: footer
    
    * update: section keyvisual
    
    * update: set main color to the var file
    
    * fix: sp画像はspへ
    
    * fix: name
    
    * update: present
    
    * fix: sp keyvid
    
    * delete: 不要のため
    
    * update: 全セクション読み込み
    
    * create: 表示切り替え用SCSS
    
    * update: breakpoint
    
    * update: contents 表示用
    
    * update: component読み込み
    
    * update: style files
    
    * update: apply sp
    
    * create: content scss
    
    * update: about sp
    
    * fix: button
    
    * create: setting pug
    
    * update: about setting 適用
    
    * fix: img path
    
    * update: special contents except GIF
    
    * update: sp content
    
    * update: terms
    
    * update: link
    
    * fix: space
    
    * update: balloon
    
    * update: quiz
    
    * fix: jsはビルドしない
    
    * add: fonts
    
    * update: about pc
    
    * fix: blockだとややこしい
    
    * create: img 4 pc
    
    * fix: 大見出し
    
    * fix: space
    
    * update: button hover
    
    * fix: after close modal
    
    * update: apply pc
    
    * update: fonts
    
    * update: kv pc
    
    * updatE: present pc
    
    * fix: 軽微な
    
    * fix: 一旦やめる
    
    * fix: about layout
    
    * fix: 指摘1
    
    * fix: present 大見出し
    
    * fix:  footer
    
    * add: remodal
    
    * update: js file
    
    * fix: present 位置
    
    * update: pc版wine
    
    * fix: base scss
    
    * fix: pc レイアウト
    
    * fix: js sp
    
    * fix: special content
    
    * Empty-Commit
    
    * fix: pssbly undf
    
    * fix: shitty rule
    
    * fix: media query
    
    * fix: footer
    
    * fix: 2push
    
    * fix: sp ピクちゃん余白
    
    * pc: bg
    
    * fix: no 10
    
    * fix: no 11
    
    * fix: no10
    
    * fix: modal fontsize

---
+++ src/assets/scripts/main.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
+import $ from 'jquery'
+import 'remodal'
+
+const ua = navigator.userAgent
+const IS_MOBILE = ua.indexOf('iPhone') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)
+
+$(() => {
+  $(document).on('click', '.js-open-answer', () => {
+    $('.js-answer').slideToggle('active')
+  })
+
+  $(document).on('closed', '.remodal', () => {
+    $('.js-answer').hide()
+  })
+  $(document).on('click', '.js-quiz-area', () => {
+    $('.js-icon').hide()
+  })
+  $(window).on('scroll', toggle)
+
+  const stopAt = $('.js-wine-stop')
+  const wine = $('.js-wine')
+  const wineHeight = wine.outerHeight() // ワインの高さ
+  const distanceFromWineTop = wine.offset().top //画面上端からワイン上端までの距離
+  const positonByWine = wineHeight + distanceFromWineTop // ワインの下端の位置
+
+  function toggle() {
+    // stopクラス付け外しの処理
+    const scrollTopWindow = $(window).scrollTop()
+    const border = stopAt.offset().top + stopAt.outerHeight()
+    console.log(scrollTopWindow)
+    if (IS_MOBILE) {
+      const kvArea = $('.js-wine-opacity').offset().top
+      const kv = $('.js-kv-area')
+      if (scrollTopWindow + positonByWine >= kvArea) {
+        kv.addClass('is-opacity')
+        kv.removeClass('is-clearly')
+        wine.addClass('is-opacity')
+        wine.removeClass('is-clearly')
+      }
+      if (scrollTopWindow < 1) {
+        if (wine.hasClass('is-opacity')) {
+          kv.addClass('is-clearly')
+          kv.removeClass('is-opacity')
+          wine.addClass('is-clearly')
+          wine.removeClass('is-opacity')
+        }
+      }
+    }
+
+    // スクロール量とワインの下端が停止位置に達した場合の処理
+    if (scrollTopWindow + positonByWine >= border) {
+      if (!stopAt.hasClass('is-stop')) {
+        stopAt.addClass('is-stop')
+      }
+    } else {
+      // 再びフローティングに戻す処理
+      if (stopAt.hasClass('is-stop')) {
+        stopAt.removeClass('is-stop')
+      }
+    }
+  }
+})
