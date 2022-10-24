f7d94e906df7863e25016bd26532bddc2b127c43
sarai2525 <s.arai@newell-productions.com>
2022-10-24 20:53:45 +0900

    fix: js sp

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -1,31 +1,54 @@
 import $ from 'jquery'
 import 'remodal'
 
-$(document).on('click', '.js-open-answer', () => {
-  $('.js-answer').slideToggle('active')
-})
-
-$(document).on('closed', '.remodal', () => {
-  $('.js-answer').hide()
-})
-$(document).on('click', '.js-quiz-area', () => {
-  $('.js-icon').hide()
-})
+const ua = navigator.userAgent
+const IS_MOBILE = ua.indexOf('iPhone') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)
 
 $(() => {
+  $(document).on('click', '.js-open-answer', () => {
+    $('.js-answer').slideToggle('active')
+  })
+
+  $(document).on('closed', '.remodal', () => {
+    $('.js-answer').hide()
+  })
+  $(document).on('click', '.js-quiz-area', () => {
+    $('.js-icon').hide()
+  })
+  $(window).on('scroll', toggle)
+
   const stopAt = $('.js-wine-stop')
   const wine = $('.js-wine')
-  const wineHeight: number = wine.outerHeight() // バナーの高さ
-  const distanceFromWineTop: number = wine.offset().top //画面上端からバナー上端までの距離
-  const positonByWine: number = wineHeight + distanceFromWineTop // バナーの下端の位置
+  const wineHeight = wine.outerHeight() // ワインの高さ
+  const distanceFromWineTop = wine.offset().top //画面上端からワイン上端までの距離
+  const positonByWine = wineHeight + distanceFromWineTop // ワインの下端の位置
 
   function toggle() {
     // stopクラス付け外しの処理
-    const scrollTopWindow: number = $(window).scrollTop()
-    const border: number = stopAt.offset().top + stopAt.outerHeight()
+    const scrollTopWindow = $(window).scrollTop()
+    const border = stopAt.offset().top + stopAt.outerHeight()
+    console.log(scrollTopWindow)
+    if (IS_MOBILE) {
+      const kvArea = $('.js-wine-opacity').offset().top
+      const kv = $('.js-kv')
+      if (scrollTopWindow + positonByWine >= kvArea) {
+        kv.addClass('is-opacity')
+        kv.removeClass('is-clearly')
+        wine.addClass('is-opacity')
+        wine.removeClass('is-clearly')
+      }
+      if (scrollTopWindow < 1) {
+        if (wine.hasClass('is-opacity')) {
+          kv.addClass('is-clearly')
+          kv.removeClass('is-opacity')
+          wine.addClass('is-clearly')
+          wine.removeClass('is-opacity')
+        }
+      }
+    }
 
+    // スクロール量とワインの下端が停止位置に達した場合の処理
     if (scrollTopWindow + positonByWine >= border) {
-      // スクロール量とバナーの下端が停止位置に達した場合の処理
       if (!stopAt.hasClass('is-stop')) {
         stopAt.addClass('is-stop')
       }
@@ -36,6 +59,4 @@ $(() => {
       }
     }
   }
-
-  $(window).on('scroll', toggle)
 })
