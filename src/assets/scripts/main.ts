a79f968ea403cf52e5a56a98158de0191282603b
sarai2525 <s.arai@newell-productions.com>
2022-10-24 19:26:53 +0900

    update: pc版wine

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -11,3 +11,31 @@ $(document).on('closed', '.remodal', () => {
 $(document).on('click', '.js-quiz-area', () => {
   $('.js-icon').hide()
 })
+
+$(() => {
+  const stopAt = $('.js-wine-stop')
+  const wine = $('.js-wine')
+  const wineHeight: number = wine.outerHeight() // バナーの高さ
+  const distanceFromWineTop: number = wine.offset().top //画面上端からバナー上端までの距離
+  const positonByWine: number = wineHeight + distanceFromWineTop // バナーの下端の位置
+
+  function toggle() {
+    // stopクラス付け外しの処理
+    const scrollTopWindow: number = $(window).scrollTop()
+    const border: number = stopAt.offset().top + stopAt.outerHeight()
+
+    if (scrollTopWindow + positonByWine >= border) {
+      // スクロール量とバナーの下端が停止位置に達した場合の処理
+      if (!stopAt.hasClass('is-stop')) {
+        stopAt.addClass('is-stop')
+      }
+    } else {
+      // 再びフローティングに戻す処理
+      if (stopAt.hasClass('is-stop')) {
+        stopAt.removeClass('is-stop')
+      }
+    }
+  }
+
+  $(window).on('scroll', toggle)
+})
