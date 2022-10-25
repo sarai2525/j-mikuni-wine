5b3fc18cacfe1f51ad2260a04627ada19d418716
sarai2525 <s.arai@newell-productions.com>
2022-10-26 03:24:30 +0900

    fix: special contents

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -2,36 +2,31 @@
 // @ts-nocheck
 
 import $ from 'jquery'
-import 'remodal'
+import Bubble from './module/bubble'
+import Modal from './module/modal'
+import NotifyIcon from './module/notifyIcon'
 
 const ua = navigator.userAgent
-const IS_MOBILE = ua.indexOf('iPhone') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)
+const isMobile = ua.indexOf('iPhone') > 0 || (ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0)
 
 $(() => {
-  $(document).on('click', '.js-open-answer', () => {
-    $('.js-answer').slideToggle('active')
-  })
+  if (isMobile) {
+    new NotifyIcon()
+  }
+  new Bubble()
+  new Modal()
 
-  $(document).on('closed', '.remodal', () => {
-    $('.js-answer').hide()
-  })
-  $(document).on('click', '.js-quiz-area', () => {
-    $('.js-icon').hide()
-  })
   $(window).on('scroll', toggle)
-
   const stopAt = $('.js-wine-stop')
   const wine = $('.js-wine')
   const wineHeight = wine.outerHeight() // ワインの高さ
   const distanceFromWineTop = wine.offset().top //画面上端からワイン上端までの距離
   const positonByWine = wineHeight + distanceFromWineTop // ワインの下端の位置
-
   function toggle() {
     // stopクラス付け外しの処理
     const scrollTopWindow = $(window).scrollTop()
     const border = stopAt.offset().top + stopAt.outerHeight()
-    console.log(scrollTopWindow)
-    if (IS_MOBILE) {
+    if (isMobile) {
       const kvArea = $('.js-wine-opacity').offset().top
       const kv = $('.js-kv-area')
       if (scrollTopWindow + positonByWine >= kvArea) {
@@ -49,7 +44,6 @@ $(() => {
         }
       }
     }
-
     // スクロール量とワインの下端が停止位置に達した場合の処理
     if (scrollTopWindow + positonByWine >= border) {
       if (!stopAt.hasClass('is-stop')) {
